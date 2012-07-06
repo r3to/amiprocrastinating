e17403f3fd279d6837610e4131d05744d3b3430e
Reto Lehnherr <reto@retolehnherr.ch>
2012-07-06 11:26:24 +0200

    Corrected a typo in the <head>'

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title>Am I Procrastinating?</title>
-  <meta name="description" content="This websites reliably tells you if you're procrastinating at the moment.'">
+  <meta name="description" content="This websites reliably tells you if you're procrastinating at the moment.">
 
   <meta name="viewport" content="width=device-width">
   <link rel="stylesheet" href="css/style.css">
